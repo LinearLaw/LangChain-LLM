80176e7c5395e5ec37a1142f84aaa2eb03b4da56
LinearLaw <rochelake@163.com>
2025-07-08 23:06:58 +0800

    【2025-07-08 23:06:38】add AiServices;

--- langchain4j-01-multi-model/src/main/java/com/linear/langchain/config/LLMConfig.java
+++ langchain4j-01-multi-model/src/main/java/com/linear/langchain/config/LLMConfig.java
@@ -1,7 +1,10 @@
 package com.linear.langchain.config;
 
+import com.linear.langchain.service.ChatAssistant;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +27,14 @@ public class LLMConfig {
                 .modelName("deepseek-r1")
                 .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
                 .build();
+    }
+
+    /**
+     * High Level API 需要用AiService.create创建
+     */
+    @Bean
+    public ChatAssistant createAiService(@Qualifier("qwen") ChatModel chatModel){
+        return AiServices.create(ChatAssistant.class, chatModel);
     }
 
 }
---
+++ langchain4j-01-multi-model/src/main/java/com/linear/langchain/controller/AiServiceController.java
@@ -0,0 +1,21 @@
+package com.linear.langchain.controller;
+
+
+import com.linear.langchain.service.ChatAssistant;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AiServiceController {
+
+    @Resource
+    private ChatAssistant assistant;
+
+
+    @GetMapping("/chat/assistant")
+    public String chatWithAssistant(@RequestParam(name = "ask", defaultValue = "你是谁") String ask){
+        return assistant.chat(ask);
+    }
+}
