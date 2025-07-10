845a3413429044543f0b6867f08ae2dcbaf525b1
LinearLaw <rochelake@163.com>
2025-07-10 20:52:17 +0800

    【2025-07-10 20:52:04】steam chat model;

---
+++ langchain4j-03-stream/src/main/java/com/linear/langchain/controller/StreamController.java
@@ -0,0 +1,16 @@
+package com.linear.langchain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stream")
+public class StreamController {
+
+    /**
+     * 1、use Flux and chatModel lower API
+     * 2、use lower API only
+     * 3、use high API: include Flux and chatModel
+     */
+
+}
