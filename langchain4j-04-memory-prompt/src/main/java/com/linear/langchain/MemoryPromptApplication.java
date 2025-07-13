c2611d0f6de62d6989aafdb8a0a19b4b3767a7c4
LinearLaw <rochelake@163.com>
2025-07-13 17:17:03 +0800

    【2025-07-13 17:16:37】chat momory and prompt;

---
+++ langchain4j-04-memory-prompt/src/main/java/com/linear/langchain/MemoryPromptApplication.java
@@ -0,0 +1,11 @@
+package com.linear.langchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryPromptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryPromptApplication.class);
+    }
+}
