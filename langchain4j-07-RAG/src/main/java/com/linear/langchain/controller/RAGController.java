961b72a6a8947744aacbb203b6747e2bdd1f47e3
LinearLaw <rochelake@163.com>
2025-07-16 20:38:45 +0800

    【2025-07-16 20:38:40】;

--- langchain4j-07-RAG/src/main/java/com/linear/langchain/controller/RAGController.java
+++ langchain4j-07-RAG/src/main/java/com/linear/langchain/controller/RAGController.java
@@ -1,6 +1,8 @@
 package com.linear.langchain.controller;
 
 import com.linear.langchain.service.ChatAssistant;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,9 @@ public class RAGController {
 
     @Resource
     private ChatAssistant chatAssistant;
+
+    @Resource
+    private InMemoryEmbeddingStore<TextSegment> inMemoryEmbeddingStore;
 
     @GetMapping("search")
     public String search(@RequestParam("ask") String ask){
